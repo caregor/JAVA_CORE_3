7e64ef48b2ebcb89012123a79790cf9555397f12
caregor <64469336+caregor@users.noreply.github.com>
2024-01-21 15:11:55 +0500

    Task 1 is done. Added method comapreDates.

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -28,6 +28,18 @@ public class Employee {
     public void increaseSalary(int value) {
         this.salary = salary.add(BigDecimal.valueOf(value));
     }
+    /**
+     * Сравнивает две даты, представленные в виде строк в формате "yyyy-mm-dd".
+     *
+     * @param date1 Первая дата для сравнения, представленная в виде строки в формате "yyyy-mm-dd".
+     * @param date2 Вторая дата для сравнения, представленная в виде строки в формате "yyyy-mm-dd".
+     * @return Отрицательное целое число, если date1 меньше date2, ноль, если date1 равна date2, или положительное целое число, если date1 больше date2.
+     */
+    public int compareDates(String date1, String date2) {
+        LocalDate localDate1 = LocalDate.parse(date1);
+        LocalDate localDate2 = LocalDate.parse(date2);
+        return localDate1.compareTo(localDate2);
+    }
 
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        Employee worker = new Employee();
+        System.out.println(worker.compareDates("2012-12-21", " "));
     }
 }
